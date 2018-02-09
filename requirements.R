4cc1bd021fef2a3b5d00047d0d2d87021171a75c
annesel <annes@ut.ee>
2018-02-09 11:18:04 +0200

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,7 +3,8 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 # install_version("ggplot2", "2.2.1")
-# install_version("dplyr", "0.7.4")
+ install_version("dplyr", "0.7.4")
+ install_version("data.table", "1.10.4")
 # install_version("tidyr", "0.7.2")
 
 ##### Install packages in development from GitHub #####
